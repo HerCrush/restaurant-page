0c34872333f73330eb094fdee6ef85a801ce051d
nacho <nacholeon30@hotmail.com>
2021-08-09 18:06:46 -0300

    home page prototype

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("Hello world!");
+(()=>{"use strict";!function(){const e=document.createElement("h1"),t=document.createElement("h2"),n=document.createElement("img");n.setAttribute("src","https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2019/06/25/15614775255199.jpg");const o=document.createElement("p");e.textContent="Super cool restaurant",t.textContent="Come eat here pls",o.textContent="We've got lots of food come eat it, it's so good I promise",document.querySelector("#content").append(e,t,n,o)}()})();
