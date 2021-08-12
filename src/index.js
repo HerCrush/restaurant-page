c5eec632a353a2ada32433f097ed8c5e31fe36e1
nacho <nacholeon30@hotmail.com>
2021-08-12 16:27:23 -0300

    working page

--- src/index.js
+++ src/index.js
@@ -2,4 +2,33 @@ import loadHome from "./home.js";
 import loadMenu from "./menu.js";
 import loadContact from "./contact.js";
 
-loadMenu();
+const nav = document.createElement('nav');
+const homeBtn = document.createElement('button');
+const menuBtn = document.createElement('button');
+const contactBtn = document.createElement('button');
+
+homeBtn.textContent = 'HOME';
+menuBtn.textContent = 'MENU';
+contactBtn.textContent = 'CONTACT';
+
+nav.append(homeBtn, menuBtn, contactBtn);
+nav.querySelectorAll('button').forEach(btn => btn.addEventListener('click', loadTab));
+
+document.querySelector('body').prepend(nav);
+
+loadHome();
+
+function loadTab(e) {
+    document.querySelector('#content div').remove();
+    switch(e.target.textContent) {
+        case 'HOME':
+            loadHome();
+            break;
+        case 'MENU':
+            loadMenu();
+            break;
+        case 'CONTACT':
+            loadContact();
+            break;
+    }
+}
